9e7e1020c6f7dbad3fcb07e8a4f5d537a1dba3d4
Your Name <you@example.com>
2026-04-08 12:24:50 +0530

    update

---
+++ objectOperations.js
@@ -0,0 +1,66 @@
+const employeeData={
+    name: "JOHN LEE",
+    age:28,
+    department:"React Developer Engineer",
+    details:{
+        salary:50000,
+        experience:5,
+        monthly:'90 k per month',
+        savings:'35 k'
+    },
+    address:[{
+        place:'Guntur',
+        street:'NTR colony',
+        house_no:'0902'
+    }]
+}
+console.log(employeeData);
+console.log(employeeData.name);
+console.log(employeeData.details.salary);
+console.log(employeeData.address[0].place);
+
+employeeData.details.salary=60000;
+
+
+employeeData.address[0].place='Hyderabad';
+console.log(employeeData.address[0].place);
+
+console.log(employeeData);
+
+delete employeeData.name;
+delete employeeData.details.salary;
+delete employeeData.address[0].house_no;
+
+console.log(employeeData);
+
+console.log(Object.keys(employeeData));
+console.log(Object.values(employeeData));
+console.log(Object.values(employeeData.details));
+console.log(Object.hasOwn(employeeData.address[0]));
+
+console.log(Object.entries(employeeData));
+console.log("age" in employeeData);
+
+let student = {
+    name: 'Rahul',
+    skills: 'Python Full Stack Development',
+    batch: 'PFS',
+    batch_name: 'PFS-42&44',
+    branch_name: 'Vasanth Nagar',
+    greet: function (){
+        console.log('Welcome To Codegnan Your Branch is', this.branch_name)
+    }
+
+}
+function assign(){
+    console.log('Frontend Assigned Batches', this.batch_name);
+}
+console.log(student);
+//console.log.call(this.batch_name, "Global");
+student.greet();
+
+assign.apply(student);
+assign.call(student);
+let result = assign.bind(student);
+result();
+
